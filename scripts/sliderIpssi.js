e83eed967bfc16f4f35a466263c8e9fff106d40c
gregoirepuget <gregoirepuget>
2019-12-12 14:04:18 +0100

    slider

--- scripts/sliderIpssi.js
+++ scripts/sliderIpssi.js
@@ -1,6 +1,9 @@
 const sliderIpssi = document.querySelector('.sliderIpssi')
 const sliderImages = sliderIpssi.querySelectorAll('img')
 const sliderIpssiContainer =document.createElement('div')
+const sliderNext = document.createElement('div')
+const sliderPrev = document.createElement('div')
+let currentPosition = 0
 
 sliderIpssiContainer.classList.add('sliderIpssiContainer')
 
@@ -10,3 +13,29 @@ for(let i=0; i<sliderImages.length;i++)
 {
   sliderIpssiContainer.appendChild(sliderImages[i])
 }
+
+sliderNext.classList.add('nextSlide')
+sliderIpssi.appendChild(sliderNext)
+sliderNext.innerHTML='&gt;'
+
+sliderPrev.classList.add('prevSlide')
+sliderIpssi.appendChild(sliderPrev)
+sliderPrev.innerHTML='&lt;'
+
+sliderNext.addEventListener(
+  'click',
+  function()
+  {
+    currentPosition++
+    sliderIpssiContainer.style.transform='translateX('+(currentPosition * -960)+'px)'
+  }
+)
+
+sliderPrev.addEventListener(
+  'click',
+  function()
+  {
+    currentPosition--
+    sliderIpssiContainer.style.transform='translateX('+(currentPosition * -960)+'px)'
+  }
+)
